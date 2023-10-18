1d7a017356781c5a357c85177046e25fa682548e
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-10-18 08:31:20 +0200

    Unify signin and signup in one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import Auth from "./pages/Auth"
 import Profile from "./pages/Profile"
 import About from "./pages/About"
 import Header from "./components/Header"
@@ -12,8 +11,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<Auth />} />
+        <Route path="/sign-up" element={<Auth />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate, useLocation } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import {
   signInStart,
@@ -12,6 +12,10 @@ export default function SignIn() {
   const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const location = useLocation()
+  const isSignup = location.pathname === "/sign-up"
+  const apiPath = isSignup ? "signup" : "signin"
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -22,7 +26,7 @@ export default function SignIn() {
     e.preventDefault()
     try {
       dispatch(signInStart())
-      const res = await fetch("/api/auth/signin", {
+      const res = await fetch(`/api/auth/${apiPath}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -43,8 +47,19 @@ export default function SignIn() {
   }
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">
+        {isSignup ? "Sign up" : "Sign In"}
+      </h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        {isSignup && (
+          <input
+            onChange={handleChange}
+            type="text"
+            id="username"
+            placeholder="username"
+            className="border p-3 rounded-lg"
+          />
+        )}
         <input
           onChange={handleChange}
           type="email"
@@ -63,14 +78,26 @@ export default function SignIn() {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign in"}
+          {loading ? "Loading..." : isSignup ? "Sign up" : "Sign In"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Don&apos;t have an account?</p>
-        <Link to="/sign-up">
-          <span className="text-blue-700">Sign up</span>
-        </Link>
+        {isSignup ? (
+          <>
+            <p>Have an account?</p>
+            <Link to="/sign-in">
+              <span className="text-blue-700">Sign in</span>
+            </Link>
+          </>
+        ) : (
+          <>
+            {" "}
+            <p>Don&apos;t have an account?</p>
+            <Link to="/sign-up">
+              <span className="text-blue-700">Sign up</span>
+            </Link>
+          </>
+        )}
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
